ec42b539231a880ce26db6ea1b22c9f03f479002
BronsonBru <bronsonvdbroeck@gmail.com>
2022-12-21 12:08:29 +0200

    Initial Commit

---
+++ MyDrink/MyDrinkApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyDrinkApp.swift
+//  MyDrink
+//
+//  Created by Bronson van den Broeck on 2022/12/21.
+//
+
+import SwiftUI
+
+@main
+struct MyDrinkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
